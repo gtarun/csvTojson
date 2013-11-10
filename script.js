f14baa0a0451e32ad3a14e96bd384df222901da1
Tarun Gupta <tarun.gupta@venturepact.com>
2013-11-10 23:23:30 +0530

    Bug fixes and formatting of code

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-/* Written By - Tarun Gupta, VenturePact
+/*************************************************
+* Written By - Tarun Gupta, VenturePact
 * Script.js built to take csv file with following column
 * @FullName, @EmailAddress
 *
@@ -9,9 +10,9 @@
 * argv[3] = nameOfTheCompany 
 * argv[4] = filename.csv
 *
-* npm used
+* Open source npm used
 * https://github.com/wdavidw/node-csv.git
-*/
+***************************************************/
 
 var csv = require('csv');
 var fs = require('fs');
@@ -19,7 +20,7 @@ var crypto = require("crypto");
 
 // pattren for validations 
 var passwordPattern = /^[a-z0-9]+$/i;
-var companyNamePattern = /^[a-z0-9]+$/i;
+var companyNamePattern = /^[a-z0-9]+$/;
 
 //Command Line argument into an object
 var allArgs = {};
@@ -31,38 +32,43 @@ allArgs.csvFileName = process.argv[4];
 var allrows=[];
 
 //check if all arguments are available and having no errors
-if (validateArguments(process.argv)) {
+var errors = validateArguments(process.argv);
+var isErrors = errors == false ? false : true ;
+if (!isErrors) {
     var data = ConvertFileToArray(allArgs.csvFileName);
     console.error(data);
 }
+else{
+  console.error("==>There are some errors: " +errors);
+}
 
-/*
+/***************************************
  *
  *function defined for processign the csv
  *
-*/
+****************************************/
 
 //Argument validation function
 function validateArguments(args) {
-  var errors;
-  console.log(args.length);
+  var isErrors=false,
+  errorList="";
   if (args.length == 5) {
-    
-    if (passwordPattern.test(allArgs.userPassword)) {
-      errors+= "\n *Password is not Good.";
-      
+    if (!passwordPattern.test(process.argv[2])) {
+      errorList+= "\n *Password is not Good.";
+      isErrors = true;
     }
-    else if (companyNamePattern.test(allArgs.companyName)) {
-      errors+= "\n *Company Name is not Good.";
-      
+    if (!companyNamePattern.test(allArgs.companyName)) {
+      errorList+= "\n *Company Name is not Good.";
+      isErrors = true;
     }else{
       // Need to write more validation code will do it later on 
     }
   }
   else{
-    errors+= "\n *Invalid Arguments Supplied.";
+    errorList = "\n *Invalid Arguments Supplied.";
+    isErrors = true;
   }
-  return errors
+  return isErrors == true ? errorList : isErrors;
 }
 
 // function to parse csv and put in global array
@@ -104,6 +110,7 @@ function ConvertFileToArray(csvFile) {
 function saveData(allrows)
 {
   console.log("from callback "+JSON.stringify( allrows));
+  
   //Need to write the mongodb conncection and create a collection with above data
   
 }
--- script1.js
+++ script1.js
@@ -1,17 +1,18 @@
-/* Written By - Tarun Gupta, VenturePact
+/*************************************************
+* Written By - Tarun Gupta, VenturePact
 * Script.js built to take csv file with following column
 * @FullName, @EmailAddress
 *
-* Console agrgument : node script.js password nameOfTheCompany filename.csv
+* Console agrgument : node script1.js password nameOfTheCompany filename.csv
 * argv[0] = node
-* argv[1] = script.js
+* argv[1] = script1.js
 * argv[2] = password
 * argv[3] = nameOfTheCompany 
 * argv[4] = filename.csv
 *
-* npm used
+* Open source npm used
 * https://github.com/wdavidw/node-csv.git
-*/
+***************************************************/
 
 var csv = require('csv');
 var fs = require('fs');
@@ -29,19 +30,20 @@ module.exports = function(mongoose) {
   var Schema = mongoose.Schema;
 
  var representives = new Schema({
-      Email             : {type : String, required : true, index: { unique: true }} 
+      Email             : {type: String, required : true, index: { unique: true }} 
     , salt              : {type: Number, set: generateSalt}
     , hashed_password   : {type: String, required: true}
-    , FullName              : {type: String, required: true}
+    , FullName          : {type: String, required: true}
     , company_head      : {type: Boolean, default: false}
     , company_id        : {type: Schema.Types.ObjectId, required: true, ref: 'Company'} //{type: Schema.ObjectId, required: true}
     , confirmed         : {type: Boolean, default: true}
     , created_at        : {type : Date, default: Date.now}
   });
 */
+
 // pattren for validations 
 var passwordPattern = /^[a-z0-9]+$/i;
-var companyNamePattern = /^[a-z0-9]+$/i;
+var companyNamePattern = /^[a-z0-9]+$/;
 
 //Command Line argument into an object
 var allArgs = {};
@@ -53,38 +55,43 @@ allArgs.csvFileName = process.argv[4];
 var allrows=[];
 
 //check if all arguments are available and having no errors
-if (validateArguments(process.argv)) {
+var errors = validateArguments(process.argv);
+var isErrors = errors == false ? false : true ;
+if (!isErrors) {
     var data = ConvertFileToArray(allArgs.csvFileName);
     console.error(data);
 }
+else{
+  console.error("==>There are some errors: " +errors);
+}
 
-/*
+/***************************************
  *
  *function defined for processign the csv
  *
-*/
+****************************************/
 
 //Argument validation function
 function validateArguments(args) {
-  var errors;
-  console.log(args.length);
+  var isErrors=false,
+  errorList="";
   if (args.length == 5) {
-    
-    if (passwordPattern.test(allArgs.userPassword)) {
-      errors+= "\n *Password is not Good.";
-      
+    if (!passwordPattern.test(process.argv[2])) {
+      errorList+= "\n *Password is not Good.";
+      isErrors = true;
     }
-    else if (companyNamePattern.test(allArgs.companyName)) {
-      errors+= "\n *Company Name is not Good.";
-      
+    if (!companyNamePattern.test(allArgs.companyName)) {
+      errorList+= "\n *Company Name is not Good.";
+      isErrors = true;
     }else{
       // Need to write more validation code will do it later on 
     }
   }
   else{
-    errors+= "\n *Invalid Arguments Supplied.";
+    errorList = "\n *Invalid Arguments Supplied.";
+    isErrors = true;
   }
-  return errors
+  return isErrors == true ? errorList : isErrors;
 }
 
 // function to parse csv and put in global array
@@ -129,7 +136,7 @@ function ConvertFileToArray(csvFile) {
 
 function saveData(allrows)
 {
-  console.log("from callback "+JSON.stringify( allrows));
+  console.log("Resultent Data : \n "+JSON.stringify( allrows));
   /*db.representives.save(allrows, function(err, records) {
     if (err) throw err;
     console.log("record added");
@@ -138,22 +145,7 @@ function saveData(allrows)
   
   //we can uncomment them when we want to put code in Firefly
   
-  /*Company.findOne({name: company}, function(err, company){
-            attrs.company_id = company._id.toString();
-            var rep = new Rep(allrows);
-            rep.set('password.raw', req.body.password); // hash password as a virtual attribute
-            rep.save(function(err){
-              if (err){
-                if (err.code === 11000){ // if mongodb returns a duplicate error
-                  req.flash("error", "The email %s is already in use.", attrs.email);
-                }
-                res.render('reps/new');
-              } else {
-                req.flash("info", "You can now login.", attrs.email);
-              }
-            }
-  });
-  
+ /*
   
     var attrs = allrow
     , company = req.subdomain;
